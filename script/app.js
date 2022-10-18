f222c2e7b4724cd9e65a25dc879f530661759fc0
crispynoodlesoup <thepanman321@gmail.com>
2022-10-18 00:14:35 -0500

    finish!

--- script/app.js
+++ script/app.js
@@ -1,32 +1,63 @@
 const sketchContainer = document.querySelector(".grid-container");
-const rows = 40;
-const cols = 40;
+let cells;
+const units = 20;
+let size = 2;
 
-for (let i = 0; i < rows; i++) {
-    const row = document.createElement("div");
-    row.classList.toggle("row");
-    sketchContainer.appendChild(row);
-    for (let j = 0; j < cols; j++) {
-        const col = document.createElement("span");
-        col.classList.toggle("cell");
-        row.appendChild(col);
+function makeBoard() {
+    sketchContainer.textContent = "";
+    for (let i = 0; i < units*size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        sketchContainer.appendChild(row);
+        for (let j = 0; j < units*size; j++) {
+            const col = document.createElement("span");
+            col.classList.add("cell");
+            row.appendChild(col);
+        }
     }
+    cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => assignRandomColor(cell));
+    cells.forEach(cell => cell.addEventListener("mouseover", e => addHoverStyle(e, cell)));
+    
 }
 
+makeBoard();
+
 const game = document.querySelector(".etch-a-sketch");
-const cells = document.querySelectorAll(".cell");
 const button = document.querySelector("button");
 const leftKnob = document.querySelector("#left-knob");
 const rightKnob = document.querySelector("#right-knob");
+const leftKnobMark = document.querySelector("#left-knob-marker");
 const rightKnobMark = document.querySelector("#right-knob-marker");
+
+function changeSize() {
+    size *= 2;
+
+    // marker animation, right left top loop
+    if (size == 4) {
+        leftKnobMark.classList.add("knob-click");
+    } else if (size > 4) {
+        size = 1;
+        leftKnobMark.classList.add("knob-click-two");
+        leftKnobMark.classList.remove("knob-click");
+    } else {
+        leftKnobMark.classList.remove("knob-click-two");
+    }
+
+    makeBoard();
+    cells.forEach(cell => {
+        cell.style.padding = 160/(units*size) + "px";
+        console.log(cell.style.padding);
+    });
+}
 
 let hold = false;
 function toggleHold() {
     hold = !hold;
-    if(hold)
-        rightKnobMark.classList.add("knob-click");
+    if (hold)
+        rightKnobMark.classList.add("zero");
     else
-        rightKnobMark.classList.remove("knob-click");
+        rightKnobMark.classList.remove("zero");
 }
 
 let holding = false;
@@ -58,8 +89,7 @@ function cellErase(c) {
 }
 
 function updateShake(e) {
-    if(e.srcElement.className == "knob-marker") return;
-    console.log(e);
+    if (!e.srcElement.classList.contains("etch-a-sketch")) return;
     document.querySelectorAll(".touched").forEach(cell => cellErase(cell));
     if (game.classList.contains("shake-down")) {
         game.classList.remove("shake-up");
@@ -73,7 +103,6 @@ function shake() {
 }
 
 game.addEventListener("transitionend", e => updateShake(e));
-cells.forEach(cell => assignRandomColor(cell));
-cells.forEach(cell => cell.addEventListener("mouseover", e => addHoverStyle(e, cell)));
 button.addEventListener("click", shake);
 rightKnob.addEventListener("click", toggleHold);
+leftKnob.addEventListener("click", changeSize);
