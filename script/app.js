de7e252ca6373fd712db96b3b12223c4e264c4d4
crispynoodlesoup <thepanman321@gmail.com>
2022-10-17 22:45:23 -0500

    add knob animations and functional hold

--- script/app.js
+++ script/app.js
@@ -1,12 +1,12 @@
 const sketchContainer = document.querySelector(".grid-container");
-const rows = 28;
-const cols = 28;
+const rows = 40;
+const cols = 40;
 
-for(let i = 0; i < rows; i++) {
+for (let i = 0; i < rows; i++) {
     const row = document.createElement("div");
     row.classList.toggle("row");
     sketchContainer.appendChild(row);
-    for(let j = 0; j < cols; j++) {
+    for (let j = 0; j < cols; j++) {
         const col = document.createElement("span");
         col.classList.toggle("cell");
         row.appendChild(col);
@@ -16,35 +16,55 @@ for(let i = 0; i < rows; i++) {
 const game = document.querySelector(".etch-a-sketch");
 const cells = document.querySelectorAll(".cell");
 const button = document.querySelector("button");
+const leftKnob = document.querySelector("#left-knob");
+const rightKnob = document.querySelector("#right-knob");
+const rightKnobMark = document.querySelector("#right-knob-marker");
+
+let hold = false;
+function toggleHold() {
+    hold = !hold;
+    if(hold)
+        rightKnobMark.classList.add("knob-click");
+    else
+        rightKnobMark.classList.remove("knob-click");
+}
+
+let holding = false;
+window.addEventListener("mousedown", () => { holding = true });
+window.addEventListener("mouseup", () => { holding = false });
 
 function assignRandomColor(c) {
-    const grey = 254 - Math.floor(Math.random()*25);
+    const grey = 254 - Math.floor(Math.random() * 25);
     c.style.backgroundColor = `rgb(${grey}, ${grey}, ${grey})`;
 }
 
 function addHoverStyle(event, c) {
-    const grey = 100 - Math.floor(Math.random()*60);
-    c.style.backgroundColor = `rgb(${grey}, ${grey}, ${grey})`;
-    c.classList.add("touched");
-    event.stopPropagation();
+    if (hold && holding || !hold) {
+        const grey = 100 - Math.floor(Math.random() * 60);
+        c.style.backgroundColor = `rgb(${grey}, ${grey}, ${grey})`;
+        c.classList.add("touched");
+        event.stopPropagation();
+    }
 }
 
 function cellErase(c) {
-    const grey = Number.parseInt(c.style.backgroundColor.substring(4,7));
-    if(grey < 230) {
-        const randGrey = 254 - Math.floor(Math.random()*25);
+    const grey = Number.parseInt(c.style.backgroundColor.substring(4, 7));
+    if (grey < 230) {
+        const randGrey = 254 - Math.floor(Math.random() * 25);
         const newGrey = Math.min(grey * 1.25, randGrey);
         c.style.backgroundColor = `rgb(${newGrey}, ${newGrey}, ${newGrey})`;
     } else
         c.classList.remove("touched");
 }
 
-function updateShake(e) { 
+function updateShake(e) {
+    if(e.srcElement.className == "knob-marker") return;
+    console.log(e);
     document.querySelectorAll(".touched").forEach(cell => cellErase(cell));
-    if(game.classList.contains("shake-down")) {
+    if (game.classList.contains("shake-down")) {
         game.classList.remove("shake-up");
         game.classList.remove("shake-down");
-    } else if(game.classList.contains("shake-up"))
+    } else if (game.classList.contains("shake-up"))
         game.classList.add("shake-down");
 }
 
@@ -54,5 +74,6 @@ function shake() {
 
 game.addEventListener("transitionend", e => updateShake(e));
 cells.forEach(cell => assignRandomColor(cell));
-cells.forEach(cell => cell.addEventListener("mouseenter", e => addHoverStyle(e,cell)));
+cells.forEach(cell => cell.addEventListener("mouseover", e => addHoverStyle(e, cell)));
 button.addEventListener("click", shake);
+rightKnob.addEventListener("click", toggleHold);
